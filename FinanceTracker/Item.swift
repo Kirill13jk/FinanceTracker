11985383113018d203d9adb6504fef22e4bd9e2c
Kirill <onlyjesus13@icloud.com>
2024-11-05 14:49:26 +0500

    Initial Commit

---
+++ FinanceTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FinanceTracker
+//
+//  Created by prom1 on 05.11.2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ FinanceTrackerTests/FinanceTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerTests.swift
+//  FinanceTrackerTests
+//
+//  Created by prom1 on 05.11.2024.
+//
+
+import Testing
+@testable import FinanceTracker
+
+struct FinanceTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
